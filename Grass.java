6ff0271c9b03bb86c765c789fc213eb9ca5becab
Sebastian-lab <59662423+Sebastian-lab@users.noreply.github.com>
2020-01-08 09:57:03 -0800

    Add files via upload

---
+++ Grass.java
@@ -0,0 +1,28 @@
+
+public class Grass {
+int x;
+int y;
+
+public Grass(int paramX, int paramY){
+	x = paramX;
+	y = paramY;
+}
+
+public int getX() {
+	return x;
+}
+
+public void setX(int x) {
+	this.x = x;
+}
+
+public int getY() {
+	return y;
+}
+
+public void setY(int y) {
+	this.y = y;
+}
+
+	
+}
