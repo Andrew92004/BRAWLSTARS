5f8d9e45a6be407b5f6a019a7f9ebadf8e13e1ce
Sebastian-lab <59662423+Sebastian-lab@users.noreply.github.com>
2020-01-08 10:23:55 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,4 +1,14 @@
 
 public class Driver {
-
+ 
+  
+  g.setColor(new Color GREEN);
+  g.drawRectangle(0,0,816,15840);
+  int[][] Map = new int[17][33];
+  
+Rectangle crate = new Rectangle(crates.getX(),crates.getY(),crates.getWidth(),crates.getHeight());
+  
+  
+  
+  
 }
