5114dd3bf389b17556535b5f0e8dc473078f1de5
Sebastian-lab <59662423+Sebastian-lab@users.noreply.github.com>
2020-01-09 08:22:08 -0800

    Update Crates.java

--- Crates.java
+++ Crates.java
@@ -1,4 +1,49 @@
 
 public class Crates {
 
+int x;
+int y;
+int width;
+int height;
+
+public Crates(int paramX, int paramY){
+x = paramX;
+y = paramY;	
+width = 16;
+height = 16;
+}
+
+public int getX() {
+	return x;
+}
+
+public void setX(int x) {
+	this.x = x;
+}
+
+public int getY() {
+	return y;
+}
+
+public int getWidth() {
+	return width;
+}
+
+public void setWidth(int width) {
+	this.width = width;
+}
+
+public int getHieght() {
+	return height;
+}
+
+public void setHieght(int hieght) {
+	this.height = hieght;
+}
+
+public void setY(int y) {
+	this.y = y;
+}
+//Rectangle r = new Rectangle(x, y, width, height);
+
 }
