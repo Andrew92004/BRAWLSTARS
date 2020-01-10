734f9b059681f409d41266af09c96fb7554ef1d1
Sebastian-lab <59662423+Sebastian-lab@users.noreply.github.com>
2020-01-10 09:05:34 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,14 +1,158 @@
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Point2D;
+import java.util.Vector;
 
-public class Driver {
- 
-  
-  g.setColor(new Color GREEN);
-  g.drawRectangle(0,0,816,15840);
-  int[][] Map = new int[17][33];
-  
-Rectangle crate = new Rectangle(crates.getX(),crates.getY(),crates.getWidth(),crates.getHeight());
-  
-  
-  
-  
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Driver extends JPanel implements ActionListener, KeyListener, MouseListener, MouseMotionListener {
+	/*Instructions
+	 * mouse to move
+	 * eat food and bots to get phat
+	 * space to boost
+	 * COMMENTS:
+	 * 
+	 * amazing game, the grid looks good.
+	 * good game
+	 * really cool game, looks similar to the actual game, but maybe make player and enemies color different, a little confusing
+	 * make the enemies diff color gj
+	 * the game is really accurate and  like how when you split you dash forwards.
+	 * 
+	 * 
+	 */
+	//size of jframe
+	int screen_width 	= 336*4;
+	int screen_height 	= 528/3*4;
+	int[][] Map = new int[21][33];
+	
+	
+	
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		//food
+		g.setColor(new Color(100, 231, 100));
+		g.fillRect(0,256,2000,3000);
+
+		
+
+	}
+	
+	public void update() {
+		//Player Movement
+		repaint();
+
+	}
+
+	public void updateBot(int id){
+		
+	}
+	//==================code above ===========================
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		update();
+		repaint();
+	}
+
+	public static void main(String[] arg) {
+		Driver d = new Driver();
+	}
+	public Driver(){
+		JFrame f = new JFrame();
+		f.setTitle("Agar.io");
+		f.setSize(screen_width, screen_height);
+		f.setBackground(Color.BLACK);
+		f.setResizable(false);
+		f.addKeyListener(this);
+		f.addMouseMotionListener(this);
+
+		//constructor
+		//initialize structures
+		
+
+		f.add(this);
+
+
+		t = new Timer(17,this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+	Timer t;
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		//boost stuff
+	
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		//allow to boost again once key is released
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent m) {
+		// TODO Auto-generated method stub
+		
+		//System.out.println((mouse[0]-p[0])+","+(mouse[1]-p[1]));
+	}
+
 }
+
