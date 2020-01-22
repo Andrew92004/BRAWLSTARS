0a01a23aab2fec917f87ea5b7584664028cf5afa
Sebastian-lab <59662423+Sebastian-lab@users.noreply.github.com>
2020-01-22 09:46:38 -0800

    Update Grass.java

--- Grass.java
+++ Grass.java
@@ -1,11 +1,23 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
 
 public class Grass {
-int x;
-int y;
-
-public Grass(int paramX, int paramY){
+private int x;
+private int y;
+private int width;
+private int height;
+private Image img;
+public Grass(int paramX, int paramY, String filename){
 	x = paramX;
 	y = paramY;
+	width = 16;
+	height = 16;
+	img = getImage(filename);
+	init(x,y);
 }
 
 public int getX() {
@@ -24,5 +36,48 @@ public void setY(int y) {
 	this.y = y;
 }
 
+public int getWidth() {
+	return width;
+}
+
+public void setWidth(int width) {
+	this.width = width;
+}
+
+public int getHeight() {
+	return height;
+}
+
+public void setHeight(int height) {
+	this.height = height;
+}
+public Image getImg() {
+	return img;
+}
+
+public void setImg(Image img) {
+	this.img = img;
+}
+private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
+
+// draw the affinetransform
+public void paint(Graphics g) {
+	Graphics2D g2 = (Graphics2D) g;
+	g2.drawImage(img, tx, null);
+}
+private void init(double a, double b) {
+	tx.setToTranslation(a, b);
+	tx.scale(4, 4);
+}
+private Image getImage(String path) {
+	Image tempImage = null;
+	try {
+		URL imageURL = Grass.class.getResource(path);
+		tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	return tempImage;
+}
 	
 }
