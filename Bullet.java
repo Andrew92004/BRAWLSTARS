972a01a0832537e4715f4635e565e4c66d0f0dac
Visionist <escaperecovery@gmail.com>
2020-01-23 08:28:52 -0800

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -8,44 +8,60 @@ import java.net.URL;
 
 public class Bullet {
 	private int x, y;
-	private int vel;
-	private double angle;
-	private Image img;
-	
-	public Bullet(String texture, int[] p, int v, double angle){
+	private int vx, vy, vel;
+	private double theta;
+	private int scale;
+	public Bullet(String team, int startX, int startY, int v, double angle){
 		
-		img = getImage("bea.png");
-		x = p[0];
-		y = p[1];
+		x = startX;
+		y = startY;
+		theta = 0;
 		vel = v;
 		//range = 7
-		init(p[0],p[1]);
+		scale = 4;
+		System.out.println(theta);
+	}
+	
+	
+	public int getX() {
+		return x;
 	}
 
-	
-	//DRAWING
-		private AffineTransform tx = AffineTransform.getTranslateInstance(x,y);
 
-		// draw the affinetransform
-			public void paint(Graphics g) {
-				Graphics2D g2 = (Graphics2D) g;
-				g2.drawImage(img, tx, null);
-			}
+	public int getY() {
+		return y;
+	}
 
-			private void init(double a, double b) {
-				tx.setToTranslation(a, b);
-				//tx.scale(scale, scale);
-			}
 
-			// converts image to make it drawable in paint
-			private Image getImage(String path) {
-				Image tempImage = null;
-				try {
-					URL imageURL = Shelly.class.getResource(path);
-					tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			return tempImage;
-			}
+	public int getVx() {
+		return vx;
+	}
+
+
+	public int getVy() {
+		return vy;
+	}
+
+
+	public int getVel() {
+		return vel;
+	}
+
+
+	public double getTheta() {
+		return theta;
+	}
+
+
+	public int getScale() {
+		return scale;
+	}
+
+
+	public void move(){
+		vx= -(int)(vel*Math.sin(theta));
+		vy = -(int)(vel*Math.cos(theta));
+		x+=vx;
+		y+=vy;
+	}
 }
