7b1b3b006ce3fe9491789b8a186d204068543d99
Visionist <escaperecovery@gmail.com>
2020-01-26 09:12:01 -0800

    Update Colt.java

--- Colt.java
+++ Colt.java
@@ -6,10 +6,12 @@ import java.awt.geom.AffineTransform;
 import java.net.URL;
 import java.util.ArrayList;
 
+
+
 public class Colt extends Brawler {
 	double scale;
 	int shotTimer;
-
+	
 	public Colt(int t, int[] p) {
 		super(t, p);
 		maxCharge = 4620;
@@ -20,90 +22,92 @@ public class Colt extends Brawler {
 		img = getImage("colt.png");
 		width = 128;
 		height = 128;
-		// range = 7
-		init(p[0], p[1]);
+		//range = 7
+		init(p[0],p[1]);
 	}
-
-	public void shoot(ArrayList<Bullet> bullets) {
-		if (ammo > 0) {
+	
+	
+	public void shoot(ArrayList<Bullet> bullets){
+		if (ammo>0) {
 			ammo--;
 			reload = reloadSpeed;
-			inCombat = true;
+			combatTimer = 5;
 			shotTimer = 18;
 		}
 	}
-
-	public void shotPattern(ArrayList<Bullet> bullets) {
-		if (shotTimer <= 0)
-			return;
-		if (shotTimer % 3 == 0) {
-			for (int i = 0; i < 1; i++) {
-				bullets.add(new Bullet(team, x + 64, y + 60, 12, theta, 420, 2, 0));
-			}
-		}
-		shotTimer--;
-	}
-
-	public void update(int fps, ArrayList<Bullet> bullets) {
-		if (reload > 0) {
-			reload -= 1 / fps;
-			if (reload == 0) {
+	public void update(int fps, ArrayList<Bullet> bullets){
+		if (reload>0){
+			reload-=1/(double)fps;
+			if (reload==0){
 				ammo++;
-				if (ammo != 3)
-					reload = reloadSpeed;
+				if (ammo != 3) reload=reloadSpeed;
 			}
 		}
-
-		if (HP <= 0) {
+		if (combatTimer>0){
+			combatTimer-=1/(double)fps;
+		}
+		if (combatTimer<=0){
+			heal();
+		}
+		
+		if (HP<=0){
 			HP = maxHP;
 			x = xi;
 			y = yi;
 			ammo = 3;
-			init(x, y);
+			init(x,y);
 		}
+		move();
 		shotPattern(bullets);
-
 	}
-	// MOVEMENT
-
-	public void move() {
-		tx.translate(vy * Math.cos(Math.PI / 2 + theta) / scale, vy * Math.sin(Math.PI / 2 + theta) / scale);
-		tx.translate(vx * Math.cos(theta) / scale, vx * Math.sin(theta) / scale);
-		x += vx;
-		y += vy;
+	public void shotPattern(ArrayList<Bullet> bullets){
+		if (shotTimer<=0) return;
+		if (shotTimer%3==0){
+			for (int i = 0; i < 1; i++){
+				bullets.add(new Bullet(team,x+64,y+60,15,theta,420,2,0));
+			}
+		}
+		shotTimer --;
 	}
-
-	public void spin(double a) {
-		// By @ArkyLi
+	//MOVEMENT
+	
+	public void move(){
+		tx.translate(vy*Math.cos(Math.PI/2+theta)/scale,vy*Math.sin(Math.PI/2+theta)/scale);
+		tx.translate(vx*Math.cos(theta)/scale,vx*Math.sin(theta)/scale);
+		x+=vx;
+		y+=vy;
+	}
+	
+	public void spin(double a){
+		//By @ArkyLi
 		double oldangle = theta;
-		theta = a;
-		tx.rotate(oldangle - theta, width / 2 / scale, height / 2 / scale);
+		theta=a;
+		tx.rotate(oldangle-theta, width/2/scale, height/2/scale);
 	}
-
-	// DRAWING
+	//DRAWING
 	private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
 
 	// draw the affinetransform
-	public void paint(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
+		public void paint(Graphics g) {
+			Graphics2D g2 = (Graphics2D) g;
 			g2.drawImage(img, tx, null);
-	}
-
-	protected void init(double a, double b) {
-		tx.setToTranslation(a, b);
-		tx.scale(scale, scale);
-	}
-
-	// converts image to make it drawable in paint
-	protected Image getImage(String path) {
-		Image tempImage = null;
-		try {
-			URL imageURL = Shelly.class.getResource(path);
-			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
-		return tempImage;
-	}
 
+		protected void init(double a, double b) {
+			tx.setToTranslation(a, b);
+			tx.scale(scale, scale);
+		}
+
+		// converts image to make it drawable in paint
+		protected Image getImage(String path) {
+			Image tempImage = null;
+			try {
+				URL imageURL = Shelly.class.getResource(path);
+				tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		return tempImage;
+		}
+	
 }
